280107755d97fe639738380d6f541f9a460510ab
Богдан Олейников <ol.bogdan13@gmail.com>
2022-01-24 12:17:59 +0200

    Fixed

--- Program.cs
+++ Program.cs
@@ -6,6 +6,55 @@ namespace Forth_Homework
     {
         private static void Main(string[] args)
         {
+            int[] array = GetRandomArray();
+
+            string evenStr = string.Empty;
+            string oddStr = string.Empty;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    FillStringCheckingLetters(ref evenStr, array[i]);
+                }
+                else
+                {
+                    FillStringCheckingLetters(ref oddStr, array[i]);
+                }
+            }
+
+            int evenStrCount = CalculateUpperLetterCount(evenStr);
+            int oddStrCount = CalculateUpperLetterCount(oddStr);
+            if (evenStrCount > oddStrCount)
+            {
+                Console.WriteLine("В массиве с парными буквами больше элементов верхнего регистра");
+            }
+            else if (evenStrCount < oddStrCount)
+            {
+                Console.WriteLine("В массиве с не парными буквами больше элементов верхнего регистра");
+            }
+            else if (evenStrCount == oddStrCount)
+            {
+                Console.WriteLine("Равное количество элементов верхнего регистра");
+            }
+
+            Console.Write("Парные: ");
+            for (int i = 0; i < evenStr.Length; i++)
+            {
+                Console.Write($"{evenStr[i]} ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Не парные: ");
+            for (int i = 0; i < oddStr.Length; i++)
+            {
+                Console.Write($"{oddStr[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static int[] GetRandomArray()
+        {
             int n = default;
             Console.Write("Введите колличество элементов массива: ");
             string str = Console.ReadLine();
@@ -14,6 +63,7 @@ namespace Forth_Homework
                 Console.WriteLine("Вы ввели не число");
                 Console.Write("Введите колличество элементов массива: ");
                 str = Console.ReadLine();
+                Console.Clear();
             }
 
             int[] array = new int[n];
@@ -23,52 +73,10 @@ namespace Forth_Homework
                 array[i] = rand.Next(1, 26);
             }
 
-            string parStr = string.Empty;
-            string notParStr = string.Empty;
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] % 2 == 0)
-                {
-                    FillStringCheckingLetters(ref parStr, array[i]);
-                }
-                else
-                {
-                    FillStringCheckingLetters(ref notParStr, array[i]);
-                }
-            }
-
-            int parStrCount = CalculateUpperLetterCount(parStr);
-            int notParStrCount = CalculateUpperLetterCount(notParStr);
-            if (parStrCount > notParStrCount)
-            {
-                Console.WriteLine("В массиве с парными буквами больше элементов верхнего регистра");
-            }
-            else if (parStrCount < notParStrCount)
-            {
-                Console.WriteLine("В массиве с не парными буквами больше элементов верхнего регистра");
-            }
-            else if (parStrCount == notParStrCount)
-            {
-                Console.WriteLine("Равное количество элементов верхнего регистра");
-            }
-
-            Console.Write("Парные: ");
-            for (int i = 0; i < parStr.Length; i++)
-            {
-                Console.Write($"{parStr[i]} ");
-            }
-
-            Console.WriteLine();
-            Console.Write("Не парные: ");
-            for (int i = 0; i < notParStr.Length; i++)
-            {
-                Console.Write($"{notParStr[i]} ");
-            }
-
-            Console.WriteLine();
+            return array;
         }
 
-        private static void FillStringCheckingLetters(ref string parOrNotParStr, int index)
+        private static void FillStringCheckingLetters(ref string evenOrOddStr, int index)
         {
             string alphabet = "abcdefghijklmnopqrstuvwxyz";
             string checkingLetters = "aeidhj";
@@ -76,12 +84,12 @@ namespace Forth_Homework
             {
                 if (alphabet[index] == checkingLetters[i])
                 {
-                    parOrNotParStr += char.ToUpper(alphabet[index]);
+                    evenOrOddStr += char.ToUpper(alphabet[index]);
                     break;
                 }
                 else if (i == checkingLetters.Length - 1)
                 {
-                    parOrNotParStr += alphabet[index];
+                    evenOrOddStr += alphabet[index];
                 }
             }
         }
